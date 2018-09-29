99bd9d05765f95a88bbb0261b5f6f74625c2cb09
Luis Daniel <luiyicpu@hotmail.com>
2018-09-29 19:59:57 -0400

    Added a config file for database connection Connection code refactoring

--- models/connection.js
+++ models/connection.js
@@ -1,13 +1,15 @@
+var path = require('path');
+var nconf = require('nconf');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var url = 'mongodb://ci-mastery-user:ci-mastery-password-6@ds213183.mlab.com:13183/ci-mastery';
-var dbName = 'ci-mastery';
+nconf.argv().env().file({file: path.join(__dirname, '..', 'config.json')});
+var mongoConfig = nconf.get('development').mongoConfig;
 
 var connect = new Promise((resolve, reject) => {
-	MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+	MongoClient.connect(mongoConfig.url, {useNewUrlParser: true}, function(err, client) {
 		assert.equal(null, err);
 		console.log("Connected successfully to Mlab Database Server");
-		resolve(client.db(dbName));
+		resolve(client.db(mongoConfig.dbName));
 		//client.close();
 	});
 });
