8bc94bee2806d9dfe123059579ef220ee2f20087
Luis Daniel <luiyicpu@hotmail.com>
2018-09-29 18:10:43 -0400

    Added test in package json Added production mode

--- test/test.js
+++ test/test.js
@@ -1,29 +1,35 @@
-/**
- * Created by Alejandro on 25/09/2018.
- */
 var supertest = require("supertest");
 var should = require("should");
+var assert = require('assert');
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:80");
+var server = supertest.agent("http://localhost:9000");
 
 // UNIT test begin
 
-describe("SAMPLE unit test",function(){
+describe('Array', function () {
+	describe('#indexOf()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal([1, 2, 3].indexOf(4), -1);
+		});
+	});
+});
 
-    // #1 should return home page
-    it("should return home page",function(done){
-        // calling home page
-        server
-            .get("/")
-            .expect("Content-type",/text/)
-            .expect(200) // THis is HTTP response
-            .end(function(err,res){
-                // HTTP status should be 200
-                res.status.should.equal(200);
-                done();
-            });
-    });
+describe("SAMPLE unit test", function () {
+
+	// #1 should return home page
+	it("should return home page", function (done) {
+		// calling home page
+		server
+			.get("/")
+			.expect("Content-type", /text/)
+			.expect(200) // THis is HTTP response
+			.end(function (err, res) {
+				// HTTP status should be 200
+				res.status.should.equal(200);
+				done();
+			});
+	});
 
 });
